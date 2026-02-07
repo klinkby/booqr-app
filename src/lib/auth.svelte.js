24702fbdd0a990ed0d57a6fc90cc28137c581b63
klinkby <1681112+klinkby@users.noreply.github.com>
2026-02-07 22:17:10 +0100

    feat: add sub

--- src/lib/auth.svelte.js
+++ src/lib/auth.svelte.js
@@ -9,6 +9,7 @@ class AuthState {
 	isLoggedIn = $derived(this.#token !== null);
 	
 	role = $derived(this.#token !== null ? parseToken(this.#token).role : null);
+	sub = $derived(this.#token !== null ? parseToken(this.#token).sub : null);
 	isEmployee = $derived(this.role === 'Employee' || this.role === 'Admin');
 
 	get accessToken() {
