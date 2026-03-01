30e21f71dce350fd84da20f9e2beabdc26fd3f97
Mads Breusch Klinkby <1681112+klinkby@users.noreply.github.com>
2026-03-01 18:01:38 +0100

    Update api (#63)
    * feat: update api
    
    * fix: paths in lighttpd.conf

--- src/lib/api/models/AddLocationRequest.js
+++ src/lib/api/models/AddLocationRequest.js
@@ -1,6 +1,10 @@
 /**
  * @typedef {object} AddLocationRequest
  * @property {string} name
+ * @property {string | null} address1
+ * @property {string | null} address2
+ * @property {string | null} zip
+ * @property {string | null} city
  */
 
 export {};
--- src/lib/api/services/VacancyService.js
+++ src/lib/api/services/VacancyService.js
@@ -1,93 +1,89 @@
-import {OpenAPI} from '../core/OpenAPI';
-import {request as __request} from '../core/request';
-
+import { OpenAPI } from '../core/OpenAPI';
+import { request as __request } from '../core/request';
 export class VacancyService {
-	/**
-	 * List vacancies
-	 * Vacancy
-	 * @param fromTime
-	 * @param toTime
-	 * @param start
-	 * @param num
-	 * @returns CollectionResponseOfCalendarEvent OK
-	 * @throws ApiError
-	 */
-	static getVacancies(fromTime, toTime, start, num) {
-		return __request(OpenAPI, {
-			method: 'GET',
-			url: '/api/vacancies',
-			query: {
-				'FromTime': fromTime,
-				'ToTime': toTime,
-				'Start': start,
-				'Num': num,
-			},
-			errors: {
-				400: `Bad Request`,
-			},
-		});
-	}
-
-	/**
-	 * Add a vacancy
-	 * Vacancy
-	 * @param requestBody
-	 * @returns CreatedResponse Created
-	 * @throws ApiError
-	 */
-	static addVacancy(requestBody) {
-		return __request(OpenAPI, {
-			method: 'POST',
-			url: '/api/vacancies',
-			body: requestBody,
-			mediaType: 'application/json',
-			errors: {
-				400: `Bad Request`,
-				401: `Unauthorized`,
-			},
-		});
-	}
-
-	/**
-	 * Get a single vacancy
-	 * Vacancy
-	 * @param id
-	 * @returns CalendarEvent OK
-	 * @throws ApiError
-	 */
-	static getVacancyById(id) {
-		return __request(OpenAPI, {
-			method: 'GET',
-			url: '/api/vacancies/{id}',
-			path: {
-				'id': id,
-			},
-			errors: {
-				400: `Bad Request`,
-				404: `Not Found`,
-			},
-		});
-	}
-
-	/**
-	 * Delete a vacancy
-	 * Vacancy
-	 * @param id
-	 * @returns void
-	 * @throws ApiError
-	 */
-	static deleteVacancy(id) {
-		return __request(OpenAPI, {
-			method: 'DELETE',
-			url: '/api/vacancies/{id}',
-			path: {
-				'id': id,
-			},
-			errors: {
-				400: `Bad Request`,
-				401: `Unauthorized`,
-				409: `Conflict`,
-			},
-		});
-	}
+    /**
+     * List vacancies
+     * Vacancy
+     * @param fromTime
+     * @param toTime
+     * @param start
+     * @param num
+     * @returns CollectionResponseOfCalendarEvent OK
+     * @throws ApiError
+     */
+    static getVacancies(fromTime, toTime, start, num) {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/vacancies',
+            query: {
+                'FromTime': fromTime,
+                'ToTime': toTime,
+                'Start': start,
+                'Num': num,
+            },
+            errors: {
+                400: `Bad Request`,
+            },
+        });
+    }
+    /**
+     * Add a vacancy
+     * Vacancy
+     * @param requestBody
+     * @returns CreatedResponse Created
+     * @throws ApiError
+     */
+    static addVacancy(requestBody) {
+        return __request(OpenAPI, {
+            method: 'POST',
+            url: '/api/vacancies',
+            body: requestBody,
+            mediaType: 'application/json',
+            errors: {
+                400: `Bad Request`,
+                401: `Unauthorized`,
+            },
+        });
+    }
+    /**
+     * Get a single vacancy
+     * Vacancy
+     * @param id
+     * @returns CalendarEvent OK
+     * @throws ApiError
+     */
+    static getVacancyById(id) {
+        return __request(OpenAPI, {
+            method: 'GET',
+            url: '/api/vacancies/{id}',
+            path: {
+                'id': id,
+            },
+            errors: {
+                400: `Bad Request`,
+                404: `Not Found`,
+            },
+        });
+    }
+    /**
+     * Delete a vacancy
+     * Vacancy
+     * @param id
+     * @returns void
+     * @throws ApiError
+     */
+    static deleteVacancy(id) {
+        return __request(OpenAPI, {
+            method: 'DELETE',
+            url: '/api/vacancies/{id}',
+            path: {
+                'id': id,
+            },
+            errors: {
+                400: `Bad Request`,
+                401: `Unauthorized`,
+                409: `Conflict`,
+            },
+        });
+    }
 }
